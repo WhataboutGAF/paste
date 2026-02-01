636a8d04c79897cfc452123c057188cbadba9974
Glorified DUCK <glorifiedduck@gmail.com>
2026-02-01 14:37:02 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-3261920634-5629a",
   "appId": "1:198508518794:web:3d297c66d664a708cdfc73",
+  "storageBucket": "studio-3261920634-5629a.appspot.com",
   "apiKey": "AIzaSyBzK0hAx9A2tt5X2_6f12Shq8qMy71ywZM",
   "authDomain": "studio-3261920634-5629a.firebaseapp.com",
   "measurementId": "",
